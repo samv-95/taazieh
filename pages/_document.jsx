ccdc8e6aae79d14cf6b25c55eb2712d3fe2f5f79
LMS <58311648+samv-95@users.noreply.github.com>
2026-07-13 10:00:38 +0330

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,9 +5,23 @@ export default function Document() {
     <Html lang="fa" dir="rtl">
       <Head>
         <link rel="manifest" href="/manifest.json" />
+
         <meta name="theme-color" content="#0a0808" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="کتابخانه تعزیه"
+        />
+
         <link rel="icon" href="/icons/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </Head>
+
       <body>
         <Main />
         <NextScript />
